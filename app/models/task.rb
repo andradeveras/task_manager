1407e040c2eee0f34260998e037814d6ba9854b2
Leonardo Veras <andradeveras96@gmail.com>
2024-10-18 17:52:23 -0300

    Add a model for connecting to the notification API

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,21 @@
 class Task < ApplicationRecord
+  after_create :send_notification
+  after_update :send_notification
   enum status: { pending: 0, in_progress: 1, completed: 2, failed: 3 }
 
   validates :title, presence: true
   validates :url, presence: true
+
+  def send_notification
+
+    notification_data = {
+      notification: {
+        title: self.title,
+        status: self.status,
+        url: self.url
+      }
+    }
+
+    HTTParty.post('http://localhost:3002/notifications', body: notification_data.to_json, headers: { 'Content-Type' => 'application/json' })
+  end
 end
